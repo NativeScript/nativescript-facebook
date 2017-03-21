787935961d41347213ee17e6c91ecdea3f7337ee
Angel Tsvetkov <mrproper86@gmail.com>
2017-03-21 15:35:58 +0200

    ios tem fix

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -1,2 +1,40 @@
 ﻿import * as application from 'application';
+
+declare class UIResponder { };
+declare var UIApplicationDelegate: any;
+declare var FBSDKApplicationDelegate: any;
+declare var FBSDKAppEvents: any;
+declare class UIApplication { };
+declare interface UIApplicationDelegate { };
+declare class NSDictionary { };
+
+if (application.ios) {
+
+  class MyDelegate extends UIResponder implements UIApplicationDelegate {
+    public static ObjCProtocols = [UIApplicationDelegate];
+
+    applicationDidFinishLaunchingWithOptions(application: UIApplication, launchOptions: NSDictionary): boolean {
+      return FBSDKApplicationDelegate.sharedInstance().applicationDidFinishLaunchingWithOptions(application, launchOptions);
+    }
+
+    applicationOpenURLSourceApplicationAnnotation(application, url, sourceApplication, annotation) {
+      return FBSDKApplicationDelegate.sharedInstance().applicationOpenURLSourceApplicationAnnotation(application, url, sourceApplication, annotation);
+    }
+
+    applicationDidBecomeActive(application: UIApplication): void {
+      FBSDKAppEvents.activateApp();
+    }
+
+    applicationWillTerminate(application: UIApplication): void {
+      //Do something you want here
+    }
+
+    applicationDidEnterBackground(application: UIApplication): void {
+      //Do something you want here
+    }
+  }
+
+  application.ios.delegate = MyDelegate;
+}
+
 application.start({ moduleName: "main-page" });
