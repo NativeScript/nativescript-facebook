189ec1badeaf7b66df531647788c459d7dc066a5
Angel Tsvetkov <mrproper86@gmail.com>
2017-03-21 15:31:02 +0200

    ios update

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -3,52 +3,17 @@ var Facebook = require('nativescript-facebook');
 // Facebook Authentication CODE
 import * as application from "application";
 
-declare class UIResponder { };
-declare var UIApplicationDelegate: any;
-declare var FBSDKApplicationDelegate: any;
-declare var FBSDKAppEvents: any;
-declare class UIApplication { };
-declare interface UIApplicationDelegate { };
-declare class NSDictionary { };
-
 export class HelloWorldModel extends Observable {
   public message: string;
-  private fbPermissions = ["publish_actions", "public_profile", "email"];
+  private fbPermissions = ["publish_actions", "public_profile"];
 
   onLogin() {
     console.log("YES");
-    Facebook.nsFacebook.requestReadPermissions(["user_friends"], () => {
+    Facebook.nsFacebook.requestReadPermissions(["email"], () => {
       console.log("YES2");
     });
   }
   constructor() {
     super();
-    if (application.ios && !application.ios.delegate) {
-      class MyDelegate extends UIResponder implements UIApplicationDelegate {
-        public static ObjCProtocols = [UIApplicationDelegate];
-
-        applicationDidFinishLaunchingWithOptions(application: UIApplication, launchOptions: NSDictionary): boolean {
-          return FBSDKApplicationDelegate.sharedInstance().applicationDidFinishLaunchingWithOptions(application, launchOptions);
-        }
-
-        applicationOpenURLSourceApplicationAnnotation(application, url, sourceApplication, annotation) {
-          return FBSDKApplicationDelegate.sharedInstance().applicationOpenURLSourceApplicationAnnotation(application, url, sourceApplication, annotation);
-        }
-
-        applicationDidBecomeActive(application: UIApplication): void {
-          FBSDKAppEvents.activateApp();
-        }
-
-        applicationWillTerminate(application: UIApplication): void {
-          //Do something you want here
-        }
-
-        applicationDidEnterBackground(application: UIApplication): void {
-          //Do something you want here
-        }
-      }
-
-      application.ios.delegate = MyDelegate;
-    }
   }
 }
