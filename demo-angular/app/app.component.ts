f6ffbaf3ac775b0955e542f83ed64c200a87e640
Dimitar Tachev <Dimitar.Tachev@progress.com>
2017-04-03 13:16:08 +0300

    Angular demo fixes.

--- demo-angular/app/app.component.ts
+++ demo-angular/app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component } from "@angular/core";
+import { LoginResponse } from "nativescript-facebook";
 
 @Component({
     selector: "ns-app",
     templateUrl: "app.component.html",
 })
-export class AppComponent { }
+export class AppComponent { 
+    onLogin = function (error: string, loginResponse : LoginResponse) {
+        console.log("TOKEN: " + loginResponse.token);
+    }
+}
--- demo-angular/app/main.ts
+++ demo-angular/app/main.ts
@@ -1,6 +1,12 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import { platformNativeScriptDynamic } from "nativescript-angular/platform";
+import * as application from 'application';
+var nsFacebook = require('nativescript-facebook');
 
 import { AppModule } from "./app.module";
+
+application.on(application.launchEvent, function (args) {
+    nsFacebook.init("159198307916686");
+});
 
 platformNativeScriptDynamic().bootstrapModule(AppModule);
