7292e9b615d292ebefda14e072d0c99736d70afc
Dimitar Tachev <Dimitar.Tachev@progress.com>
2017-04-05 18:51:15 +0300

    Added custom login button in the angular demo.

--- demo-angular/app/app.component.ts
+++ demo-angular/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from "@angular/core";
-import { LoginResponse } from "nativescript-facebook";
+import * as Facebook from "nativescript-facebook";
 
 @Component({
     selector: "ns-app",
     templateUrl: "app.component.html",
 })
 export class AppComponent { 
-    onLogin = function (error: string, loginResponse : LoginResponse) {
+    onLogin = function (error: string, loginResponse : Facebook.LoginResponse) {
         console.log("TOKEN: " + loginResponse.token);
+    }
+
+    testAction = function () {
+        Facebook.login((error, data) => {
+            console.log("Success!");
+        });
     }
 }
