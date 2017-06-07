310cab65d5d6377837c9933cef6a46d4cffda05a
Zdravko Branzov <Zdravko.Branzov@progress.com>
2017-06-07 15:28:15 +0300

    Use one test for all platforms

--- demo/e2e-tests/tests.js
+++ demo/e2e-tests/tests.js
@@ -8,7 +8,7 @@ describe("facebook tests", function () {
     const USERNAME = "open_qgonlya_user@tfbnw.net";
     const PASSWORD = "P@ssw0rd";
     const CUSTOM_LOGOUT_BUTTON = "customLogOut";
-    const USER_ID = "UserId: 132396757312450";
+    const USER_NAME = "Open Graph Test User";
 
     before(function () {
         driver = nsAppium.createDriver();
@@ -21,78 +21,40 @@ describe("facebook tests", function () {
             console.log("Driver quit successfully");
         });
     });
-//=============================== ANDROID TESTS ==========================
-    if(isAndroid){
-        describe("android tests", function () {
 
-            it("should log in via original button", function () {
-                return driver
-                    .elementByAccessibilityId(FACEBOOK_BUTTON)
-                        .should.eventually.exist
-                    .click()
-                    .waitForElementsByClassName(nsAppium.getXPathElement("textfield"), 10000).first()
-                    .setText(USERNAME)
-                    .elementsByClassName(nsAppium.getXPathElement("textfield")).last()  //Password field
-                    .setText(PASSWORD)
-                    .elementsByClassName(nsAppium.getXPathElement("button")).first() //Log in button
-                    .click()
-                    .sleep(3000)
-                    .waitForElementsByClassName(nsAppium.getXPathElement("button"), 10000).last()  // OK button
-                    .click()
-                    .waitForElementsByClassName(nsAppium.getXPathElement("label"), 10000).last()
-                        .text().should.eventually.equal(USER_ID)
-            });
+    it("should log in via original button", function () {
+        var config = {
+            passwordFieldElement : isAndroid ? "textfield" : "securetextfield",
+            loginButtonIndex : isAndroid ? 1 : 4,
+            okButtonIndex : isAndroid ? 2 : 5
+        }
 
-            it("should log out via custom button", function () {
-                return driver
-                    .elementByAccessibilityId(CUSTOM_LOGOUT_BUTTON)
-                        .should.eventually.exist
-                    .tap()
-                    .waitForElementByAccessibilityId(FACEBOOK_BUTTON)
-                        .text().should.eventually.equal("Log in with Facebook")
-            });
-        });
-    } else {
-//=========================================== IOS TESTS ======================================
-        describe("ios tests", function(){
+        var step1 = driver
+            .elementByAccessibilityId(FACEBOOK_BUTTON)
+                .should.eventually.exist
+            .click()
+            .waitForElementsByClassName(nsAppium.getXPathElement("textfield"), 10000).first()
+            .sendKeys(USERNAME)
+            .elementsByClassName(nsAppium.getXPathElement(config.passwordFieldElement)).last()  //Password field
+            .sendKeys(PASSWORD)
+            .elementsByClassName(nsAppium.getXPathElement("button")).nth(config.loginButtonIndex) //Log in button
+            .click();
+        var step2 = isAndroid ? step1.sleep(6000) : step1.sleep(2000);
+        step2
+            .waitForElementsByClassName(nsAppium.getXPathElement("button"), 10000).nth(config.okButtonIndex)  // OK button
+            .click();
+        var step3 = isAndroid ? step2 : step2.sleep(5000);
+        return step3
+            .waitForElementsByClassName(nsAppium.getXPathElement("label"), 10000).last() //TODO use wait for element by text USER_ID
+                .text().should.eventually.equal(USER_NAME);
+    });
 
-            it("should log in via original button", function () {
-                return driver
-                    .elementByAccessibilityId(FACEBOOK_BUTTON)
-                        .should.eventually.exist
-                    .click()
-                    // Needed for IOS because IfExists does not have timeout
-                    .sleep(5000)
-                    .elementByClassNameIfExists(nsAppium.getXPathElement("textfield"))
-                    .then(function(el){
-                        if(el){
-                            return driver
-                            .elementsByClassName(nsAppium.getXPathElement("textfield")).first()
-                            .sendKeys(USERNAME)
-                            .elementsByClassName(nsAppium.getXPathElement("securetextfield")).last() // Password field
-                            .sendKeys(PASSWORD)
-                            .elementsByClassName(nsAppium.getXPathElement("button")).nth(4) //Log in button
-                            .click()
-                        }
-                    })
-                    .waitForElementsByClassName(nsAppium.getXPathElement("button"), 10000).nth(5) // OK button
-                    .click()
-                    .sleep(2000) // Take time to change label value
-                    .elementByClassName(nsAppium.getXPathElement("label"))
-                        .text().should.eventually.equal(USER_ID)
-            });
-
-            it("should log out via original button", function () {
-                return driver
-                    .elementByAccessibilityId(FACEBOOK_BUTTON)
-                        .should.eventually.exist
-                    .click()
-                    .elementsByClassName(nsAppium.getXPathElement("button")).first() //Log out confirmation button
-                    .click()
-                    .sleep(2000) // Take time to change label value
-                    .elementByClassName(nsAppium.getXPathElement("label"))
-                        .text().should.eventually.equal("not logged in")
-            });
-        })
-    }
+    it("should log out via custom button", function () {
+        return driver
+            .elementByAccessibilityId(CUSTOM_LOGOUT_BUTTON)
+                .should.eventually.exist
+            .click()
+            .elementByAccessibilityId(FACEBOOK_BUTTON)
+                .should.eventually.exist
+    });
 });
