8dd85717b63476ef829d119bfeb7177c75892a3d
gogoout <gogoout@gmail.com>
2018-12-14 18:03:44 +1300

    add demo angular

--- demo-angular/app/app.module.ts
+++ demo-angular/app/app.module.ts
@@ -1,6 +1,6 @@
-import { NgModule, NO_ERRORS_SCHEMA, NgModuleFactoryLoader } from "@angular/core";
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-import { NativeScriptRouterModule, NSModuleFactoryLoader } from "nativescript-angular/router";
+import { NativeScriptRouterModule } from "nativescript-angular/router";
 import { AppComponent } from "./app.component";
 import { LoginModule } from "./pages/login/login.module";
 import { HomeModule } from "./pages/home/home.module";
@@ -23,8 +23,7 @@ application.on(application.launchEvent, function (args) {
         NativeScriptRouterModule.forRoot(routes)
     ],
     providers: [
-        NavigationService,
-        { provide: NgModuleFactoryLoader, useClass: NSModuleFactoryLoader }
+        NavigationService
     ],
     declarations: [AppComponent],
     schemas: [NO_ERRORS_SCHEMA]
--- demo-angular/app/pages/login/login.component.ts
+++ demo-angular/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, ChangeDetectorRef } from "@angular/core";
 import * as Facebook from "nativescript-facebook";
 import { NavigationService } from "../../services/navigation.service";
 import * as appSettings from "tns-core-modules/application-settings";
+import {fromResource} from 'tns-core-modules/image-source';
 
 @Component({
     selector: "login",
@@ -9,9 +10,30 @@ import * as appSettings from "tns-core-modules/application-settings";
     templateUrl: "login.component.html",
     styleUrls: ["login.component.css"]
 })
-export class LoginComponent {
+export class LoginComponent{
+    linkContent = null;
+    photosContent = null;
+    genericContent = null;
+    canShowLinksShareDialog = false;
+    canShowPhotosShareDialog = false;
+    canShowLinksMessageDialog = false;
+    canShowGenericMessageDialog = false;
 
     constructor(private ref: ChangeDetectorRef, private navigationService: NavigationService) {
+        // have to init after facebook sdk inited
+        setTimeout(() => {
+            this.init();
+        }, 100);
+    }
+
+    init() {
+        this.linkContent = this.generateLinksShareContent();
+        this.photosContent = this.generatePhotosShareContent();
+        this.genericContent = this.generateGenericTemplateContent();
+        this.canShowLinksShareDialog = Facebook.canShareDialogShow(this.linkContent);
+        this.canShowPhotosShareDialog = Facebook.canShareDialogShow(this.photosContent);
+        this.canShowLinksMessageDialog = Facebook.canMessageDialogShow(this.linkContent);
+        this.canShowGenericMessageDialog = Facebook.canMessageDialogShow(this.genericContent);
     }
 
     onLogin(eventData: Facebook.LoginEventData) {
@@ -34,9 +56,61 @@ export class LoginComponent {
         });
     }
 
-    public getCurrentAccessToken() {
+    getCurrentAccessToken() {
         let accessToken = Facebook.getCurrentAccessToken();
 
         alert("Current access token: " + JSON.stringify(accessToken, null, '\t'));
+    }
+
+    generateLinksShareContent() {
+        return Facebook.createLinksShareContent('https://www.nativescript.org',
+            'Create Native iOS and Android Apps With JavaScript',
+            {
+                hashtag: '#Nativescript'
+            })
+    }
+
+    generatePhotosShareContent() {
+        const logoImage = fromResource('logo');
+        return Facebook.createPhotosShareContent([logoImage], false, {
+            hashtag: '#Nativescript'
+        });
+    }
+
+    generateGenericTemplateContent() {
+        return Facebook.createShareMessengerGenericTemplateContent({
+            element: {
+                title: 'Nativescript',
+                subtitle: 'Create Native iOS and Android Apps With JavaScript',
+                imageUrl: 'https://d2odgkulk9w7if.cloudfront.net/images/default-source/home/how-it-works-min.png',
+                button: {
+                    title: 'Check Doc',
+                    url: 'https://docs.nativescript.org'
+                },
+                defaultAction: {
+                    title: 'Go HomePage',
+                    url: 'https://www.nativescript.org'
+                }
+            },
+            // it seems android have to provide a pageId, otherwise the MessageDialog just wont show
+            pageID: 'testestsett',
+            imageAspectRatio: Facebook.MessageGenericTemplateImageAspectRatio.Horizontal
+        });
+    }
+
+    onShareDialog() {
+        Facebook.showShareDialog(this.linkContent);
+    }
+
+    onShareDialogPhotos() {
+        Facebook.showShareDialog(this.photosContent);
+    }
+
+    onSendDialog() {
+        Facebook.showMessageDialog(this.linkContent);
+    }
+
+    onSendGenericDialog() {
+        Facebook.showMessageDialog(this.genericContent);
     }
 }
