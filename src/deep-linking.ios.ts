87dc1d538e9f56c0dd31e869ffdfc931ab070a4d
Alex Gritton <alexgritton@gmail.com>
2020-08-25 14:29:18 -0600

    Added ios Deep link callback

--- src/deep-linking.ios.ts
+++ src/deep-linking.ios.ts
@@ -1,6 +1,7 @@
 import {
     DeepLink
 } from './deep-linking.common';
+import { ios } from 'tns-core-modules/application';
 
 export function initDeepLinking(appId: string) {
     FBSDKSettings.autoInitEnabled = true;
@@ -22,4 +23,18 @@ export function fetchDeferredAppLink(): Promise<DeepLink> {
             resolve(deepLink);
         });
     });
+}
+
+export function registerDeepLinkCallback(callback): void {
+    if (ios && ios.delegate) {
+        const crtHandler = ios.delegate.prototype["applicationOpenURLOptions"];
+        ios.delegate.prototype["applicationOpenURLOptions"] = function () {
+            const args = Array.from(arguments);
+            if (crtHandler) {
+                const result = crtHandler.apply(this, args);
+                args.push(result);
+            }
+            return callback.apply(this, args);
+        }
+    }
 }
